100e111b2f9e176048e58a865a0650dc2800b51c
Anar <anar.sol.dev@gmail.com>
2021-09-07 19:18:45 +0100

    add RGB controls to create color

--- random-color-generator.js
+++ random-color-generator.js
@@ -7,28 +7,67 @@ function generateRandomColor() {
     return { r: r, g: g, b: b };
 }
 
+function formatColor(color, type) {
+    let formatted = "";
+    if (color != null) {
+        if (type === "RGB") {
+            formatted = `rgb(${color.r}, ${color.g}, ${color.b})`;
+        } else if (type == "HSL") {
+            const colorHSL = rgbToHsl(color.r, color.g, color.b);
+            colorHSL.h = Math.round(colorHSL.h * 360);
+            colorHSL.s = Math.round(colorHSL.s * 100);
+            colorHSL.l = Math.round(colorHSL.l * 100);
+            formatted = `hsl(${colorHSL.h}, ${colorHSL.s}%, ${colorHSL.l}%)`;
+        } else {
+            const colorHex = {
+                r: color.r < 16 ? "0" + color.r.toString(16) : color.r.toString(16),
+                g: color.g < 16 ? "0" + color.g.toString(16) : color.g.toString(16),
+                b: color.b < 16 ? "0" + color.b.toString(16) : color.b.toString(16)
+            };
+            formatted = `#${colorHex.r}${colorHex.g}${colorHex.b}`;
+        }
+    }
+    return formatted;
+}
+
 const rcgColor = document.querySelector(".rcg__color");
 const rcgValueHex = document.querySelector(".rgc__hex");
 const rcgValueRGB = document.querySelector(".rgc__rgb");
 const rcgValueHSL = document.querySelector(".rgc__hsl");
 const rcgButton = document.querySelector(".rcg__btn");
+const rcgRedInput = document.querySelector("#red");
+const rcgGreenInput = document.querySelector("#green");
+const rcgBlueInput = document.querySelector("#blue");
 
-function updateColor() {
-    const colorRGB = generateRandomColor();
-    const colorHex = {r: colorRGB.r < 16 ? "0" + colorRGB.r.toString(16): colorRGB.r.toString(16),
-    g: colorRGB.g < 16 ? "0" + colorRGB.g.toString(16): colorRGB.g.toString(16),
-    b: colorRGB.b < 16 ? "0" + colorRGB.b.toString(16): colorRGB.b.toString(16)};
-    const colorHSL = rgbToHsl(colorRGB.r, colorRGB.g, colorRGB.b);
-    colorHSL.h = Math.round(colorHSL.h * 360);
-    colorHSL.s = Math.round(colorHSL.s * 100);
-    colorHSL.l = Math.round(colorHSL.l * 100);
+function updateColor(color) {
+    rcgColor.style.setProperty("--rcg-color", formatColor(color));
 
-    rcgColor.style.setProperty("--rcg-color", `rgb(${colorRGB.r}, ${colorRGB.g}, ${colorRGB.b})`);
+    rcgValueHex.textContent = formatColor(color);
+    rcgValueRGB.textContent = formatColor(color, "RGB");
+    rcgValueHSL.textContent = formatColor(color, "HSL");
 
-    rcgValueHex.textContent = `#${colorHex.r}${colorHex.g}${colorHex.b}`;
-    rcgValueRGB.textContent = `rgb(${colorRGB.r}, ${colorRGB.g}, ${colorRGB.b})`;
-    rcgValueHSL.textContent = `hsl(${colorHSL.h}, ${colorHSL.s}%, ${colorHSL.l}%)`;
+    rcgRedInput.value = color.r;
+    rcgGreenInput.value = color.g;
+    rcgBlueInput.value = color.b;
 }
 
-window.addEventListener("load", updateColor);
-rcgButton.addEventListener("click", updateColor);
+function createColor() {
+    const red = parseInt(rcgRedInput.value, 10);
+    const green = parseInt(rcgGreenInput.value, 10);
+    const blue = parseInt(rcgBlueInput.value, 10);
+
+    const color = {r: red, g: green, b: blue};
+    updateColor(color);
+}
+
+function randomColor() {
+    const color = generateRandomColor();
+    updateColor(color);
+}
+
+window.addEventListener("load", randomColor);
+rcgButton.addEventListener("click", randomColor);
+
+rcgRedInput.addEventListener("input", createColor);
+rcgGreenInput.addEventListener("input", createColor);
+rcgBlueInput.addEventListener("input", createColor);
